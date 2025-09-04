e438026bb4df51de6d6805c2b5d0e6c76f002d55
pratibhakashyap0 <pratibhakashyap2005@gmail.com>
2025-09-04 13:49:26 +0530

    Add files via upload

---
+++ practical11.php
@@ -0,0 +1,7 @@
+<?php
+// Printing a heading using HTML tag directly
+echo "<h1>Welcome to My Webpage</h1>";
+
+// Printing a paragraph using PHP
+echo "<p>This is a paragraph displayed using PHP echo statement.</p>";
+?>
